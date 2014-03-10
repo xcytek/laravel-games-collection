8985e8dacddf3d711d4dfd5d822f89f2dc17926e
Alex Acosta <alexcarter15@hotmail.com>
2014-03-10 22:04:53 +0000

    Replacing Auth::check() instead Session::has() method

--- playstationApp/app/views/layout.blade.php
+++ playstationApp/app/views/layout.blade.php
@@ -9,22 +9,23 @@
 </head>
 <body>
     <div class="container">
-        <nav class="navbar navbar-default row" role="navigation" >
-            
+        <nav class="navbar navbar-default navbar-fixed-top row" role="navigation" style="opacity:0.8;">
             <div class="col-md-6">
-                <a href="{{ action('GamesController@index') }}" class="navbar-brand">Games Collection</a>
+                {{ link_to('/', 'Games Collection', $attributes = array('class'=>'navbar-brand'), $secure = null); }}
             </div>
             
-            <div class="col-md-1" style="position:fixed; margin-left:900px;">
-                @if (Session::has('username'))
-                    <a href="{{ action('UsersController@logout') }}" class="navbar-brand btn btn-default">Logout {{ Session::get('username') }}</a>
+            <div class="col-md-6">
+                @if (Auth::check())
+                    <a href="{{ action('UsersController@logout') }}" class="navbar-brand btn btn-default  navbar-right">Logout 
+                        {{ Auth::user()->username . ' <small>'. Auth::user()->email . '</small>' }}</a>
                 @elseif (Request::is('register'))
-                    <a href="{{ action('UsersController@login') }}" class="navbar-brand">Sign In</a>
+                    <a href="{{ action('UsersController@login') }}" class="navbar-brand  navbar-right">Sign In</a>
                 @else
-                    <a href="{{ action('UsersController@register') }}" class="navbar-brand">Sign Up</a>
+                    <a href="{{ action('UsersController@register') }}" class="navbar-brand  navbar-right">Sign Up</a>
                 @endif
             </div>
         </nav>
+        <div style="margin:20px 0px">&nbsp;</div>
         @yield('content')
     </div>
 </body>
